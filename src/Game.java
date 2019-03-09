605084b9b109784c52d2ed1712e357a240255168
MARK ELLIS <mark.m.ellis@utexas.edu>
2019-03-09 00:25:38 -0600

    minor changes

--- src/Game.java
+++ src/Game.java
@@ -43,7 +43,7 @@ public class Game {
 //		theDeck.shuffle();
 //		
 		//Creating players
-		String names[] = {"Anna", "Helina", "Josh", "Daniel", "Rebecca", "Dad", "Mom", "Myself", "Sandra", "Lizzie"};
+		String names[] = {"Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10"};
 //		String names[] = {"Anna", "Helina"};
 		
 		Dealer deal = new Dealer(names, 10);
